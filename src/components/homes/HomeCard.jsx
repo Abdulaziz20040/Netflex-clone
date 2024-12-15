62904cc83d27d5c12659fc83de31b332e9a21798
Abdulaziz20040 <abduazizabdushukurov2004@gmail.com>
2024-12-15 14:43:13 +0500

    first commit

---
+++ src/components/homes/HomeCard.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomeCard = ({
+  item: { id, cover, name, rating, time, desc, starring, genres, tags, video },
+}) => {
+  return (
+    <>
+      <div className="box">
+        <div className="coverImage">
+          <img src={cover} alt="" />
+        </div>
+        <div className="content flex">
+          <div className="details row">
+            <h1>{name}</h1>
+            <div className="rating flex">
+              <div className="rate">
+                <i className="fas fa-star"></i>
+                <i className="fa fa-star"></i>
+                <i className="fa fa-star"></i>
+                <i className="fa fa-star"></i>
+                <i className="fa fa-star-half"></i>
+              </div>
+              <label>{rating}(Imdb)</label>
+              <span>GP</span>
+              <label>{time}</label>
+            </div>
+            <p>{desc}</p>
+            <div className="cast">
+              <h4>
+                <span>Starring </span>
+                {starring}
+              </h4>
+              <h4>
+                <span>Genres </span>
+                {genres}
+              </h4>
+              <h4>
+                <span>Tags </span>
+                {tags}
+              </h4>
+            </div>
+            <button className="primary-btn">
+              <i className="fas fa-play"></i> PLAY NOW
+            </button>
+          </div>
+          <div className="palyButton row"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomeCard;
